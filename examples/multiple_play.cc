446341fb8bc1f1fa7db5de6a1fa091762bd72c42
zhaoyouqing <zhaoyouqing@52tt.com>
2022-09-02 18:55:05 +0800

    add test example

--- examples/multiple_play.cc
+++ examples/multiple_play.cc
@@ -10,19 +10,30 @@
 #include "rocksdb/options.h"
 #include "rocksdb/status.h"
 
+using ROCKSDB_NAMESPACE::ColumnFamilyDescriptor;
 using ROCKSDB_NAMESPACE::ColumnFamilyHandle;
 using ROCKSDB_NAMESPACE::ColumnFamilyOptions;
 using ROCKSDB_NAMESPACE::DB;
 using ROCKSDB_NAMESPACE::Options;
+using ROCKSDB_NAMESPACE::Slice;
 using ROCKSDB_NAMESPACE::Status;
 using ROCKSDB_NAMESPACE::WriteOptions;
 
 const std::string kDBPath = "./storage/rocksdb_multiple_play";
+const uint64_t kMaxKeySize = 600000;  // 最大生成的key的数量
+
+const size_t kMaxValueLength = 256;  // key 最大长度
+const size_t kNumKeysPerFlush = 1000;
 
 const std::vector<std::string>& GetColumnFamilyNames() {
   static std::vector<std::string> column_family_names = {
       ROCKSDB_NAMESPACE::kDefaultColumnFamilyName, "youqing"};
   return column_family_names;
+}
+
+inline bool IsLittleEndian() {
+  uint32_t x = 1;
+  return *reinterpret_cast<char*>(&x) != 0;
 }
 
 void CreateDB() {
@@ -45,13 +56,14 @@ void CreateDB() {
     } else {
       std::vector<ColumnFamilyHandle*> handles;
       ColumnFamilyOptions cf_opts(options);
-      for (const auto& cf_name: GetColumnFamilyNames()) {
+      for (const auto& cf_name : GetColumnFamilyNames()) {
         if (cf_name != ROCKSDB_NAMESPACE::kDefaultColumnFamilyName) {
           ColumnFamilyHandle* handle = nullptr;
           s = db->CreateColumnFamily(cf_opts, cf_name, &handle);
           if (!s.ok()) {
-            fprintf(stderr, "[process %ld] Failed to create column family: %s\n", my_pid,
-                    s.ToString().c_str());
+            fprintf(stderr,
+                    "[process %ld] Failed to create column family: %s\n",
+                    my_pid, s.ToString().c_str());
             assert(false);
           } else {
             handles.push_back(handle);
@@ -59,12 +71,11 @@ void CreateDB() {
         }
 
         fprintf(stdout, "[process %ld] Column families created\n", my_pid);
-        for (auto h: handles) {
+        for (auto h : handles) {
           delete h;
         }
         handles.clear();
       }
-
     }
 
     delete db;
@@ -72,5 +83,81 @@ void CreateDB() {
   }
 }
 
-// clang++ multiple_play.cc  ../librocksdb.a -o a.out -I ../include/  -std=c++17 -lpthread -lz  -lbz2 -lzstd -ldl
+static std::string Key(uint64_t k) {
+  std::string ret;
+  if (IsLittleEndian()) {
+    ret.append(reinterpret_cast<char*>(&k), sizeof(k));
+  } else {
+    char buf[sizeof(k)];
+    for (size_t i = 0; i < sizeof(k); ++i) {
+      buf[i] = (k >> (i * 8)) & 0xff;
+    }
+    ret.append(buf, sizeof(k))
+  }
+  size_t i = 0;
+  size_t j = ret.size() - 1;
+  while (i < j) {
+    char tmp = ret[i];
+    ret[i] = ret[j];
+    ret[j] = tmp;
+    ++i;
+    --j;
+  }
+
+  return ret;
+}
+
+static Slice GenerateRandomValue(const size_t max_length, char scratch[]) {
+  // random size
+  int base_rand_value = std::rand();
+  size_t sz = (std::rand() % max_length) + 1;
+  for (size_t i = 0; i < sz; ++i) {
+    scratch[i] = static_cast<char>(base_rand_value ^ i);
+  }
+  return Slice(scratch, sz);
+}
+
+void RunPrimary() {
+  long my_pid = static_cast<long>(getpid());
+  fprintf(stdout, "[process %ld] Primary instance starts \n", my_pid);
+  CreateDB();
+
+  // 设置随机种子
+  std::srand(time(0));
+
+  // 初始化DB
+  Options options;
+  options.create_if_missing = true;
+  std::vector<ColumnFamilyDescriptor> column_families;
+  for (const auto& cf_name : GetColumnFamilyNames()) {
+    column_families.push_back(ColumnFamilyDescriptor(cf_name, options));
+  }
+  std::vector<ColumnFamilyHandle*> handles;
+  DB* db;
+  Status s = DB::Open(options, kDBPath, column_families, &handles, &db);
+  if (!s.ok()) {
+    fprintf(stderr, "[process %ld] Failed to open db: %s", my_pid,
+            s.ToString());
+    assert(false);
+  }
+  assert(handles.size() == GetColumnFamilyNames().size())
+
+      // todo batch write data into db
+      uint64_t cur_key_size = 0;
+  while (cur_key_size < kMaxKeySize) {
+    for (auto h : handles) {
+      assert(h != nullptr) for (size_t i = 0; i != kNumKeysPerFlush, ++i) {
+        // gen key and write into db
+        Slice key = Key(cur_key_size + static_cast<uint64_t>(i));
+        // gen value
+        //        Slice value =
+      }
+    }
+  }
+
+  Status s = DB::Open(options, kDBPath, column_families, &handles, &db);
+}
+
+// clang++ multiple_play.cc  ../librocksdb.a -o a.out -I ../include/  -std=c++17
+// -lpthread -lz  -lbz2 -lzstd -ldl
 int main() { CreateDB(); }
---
+++ examples/play.cc
@@ -0,0 +1,52 @@
+//
+// Created by zhaoyouqing on 2022/8/30.
+//
+
+#include <cstdio>
+#include <cstdint>
+#include <string>
+#include <inttypes.h>
+
+
+inline bool IsLittleEndian() {
+  uint32_t x = 1;
+  return *reinterpret_cast<char*>(&x) != 0;
+}
+
+static std::string Key(uint64_t k) {
+//  printf("k address: %p, %lu\n", &k, sizeof(k));
+  std::string ret;
+  if (IsLittleEndian()) {
+    ret.append(reinterpret_cast<char*>(&k), sizeof(k));
+//    printf("ret: %s, 111111111\n", ret.c_str());
+  } else {
+    char buf[sizeof(k)];
+    for (size_t i = 0; i < sizeof(k); ++i) {
+      buf[i] = (k >> (i * 8)) & 0xff;
+    }
+    ret.append(buf, sizeof(k));
+  }
+
+//  size_t i = 0, j = ret.size() - 1;
+//  while (i < j) {
+//    char tmp = ret[i];
+//    ret[i] = ret[j];
+//    ret[j] = tmp;
+//    ++i;
+//    --j;
+//  }
+  printf("ret: %s\n", ret.c_str());
+  return ret;
+}
+
+void test_key() {
+  for (uint64_t i = 0; i < 300; ++i) {
+    std::string str = Key(i);
+
+    fprintf(stdout, "address: %p, i: %" PRIu64 ", gen_str: %s \n", &str, i, str.c_str());
+  }
+}
+
+int main() {
+  test_key();
+}
