d55c301a6e4fcd2f6fb5ac8c2e81606f776968f5
RavenHide <610412095@qq.com>
2022-09-12 11:47:15 +0800

    add merge and transaction code

--- examples/merge_example.cc
+++ examples/merge_example.cc
@@ -2,11 +2,14 @@
 // Created by zhaoyouqing on 2022/9/7.
 //
 
-#include <error.h>
+#include "thread"
+#include "chrono"
+#include <errno.h>
 #include "iostream"
 #include "rocksdb/merge_operator.h"
 #include "rocksdb/env.h"
 #include "rocksdb/db.h"
+#include "rocksdb/utilities/db_ttl.h"
 #include "util/coding_lean.h"
 
 using ROCKSDB_NAMESPACE::AssociativeMergeOperator;
@@ -92,35 +95,98 @@ void testStringConvertToUint64() {
 }
 
 // build cmd
-//
+// linux
 // g++-7 merge_example.cc ../librocksdb.a -o merge_example.out -I ../include/ -I ../ -std=c++17 -lpthread -lz  -lbz2 -lzstd -ldl -lsnappy -llz4  -DROCKSDB_PLATFORM_POSIX -fno-rtti
+//
+// macos
+// clang++ merge_example.cc ../librocksdb.a -o merge_example.out -I ../include/ -I ../ -std=c++17 -lpthread -lz  -lbz2 -lzstd -ldl   -DROCKSDB_PLATFORM_POSIX -fno-rtti -DOS_MACOSX
 
-int main() {
-
-  // open db and merge
-  rocksdb::DB* db;
+void playTtlDB() {
+  const std::string path = "storage/ttl/";
   rocksdb::Options opt;
   opt.create_if_missing = true;
-  opt.merge_operator.reset(new Uint64AddOperator);
-  rocksdb::Status s = rocksdb::DB::Open(opt, "storage/merge_example", &db);
+  rocksdb::DBWithTTL* db = nullptr;
+  rocksdb::Status s = rocksdb::DBWithTTL::Open(opt, path, &db, 2, false);
   if (!s.ok()) {
-    fprintf(stderr, "failed to open db, status: %s\n", s.ToString().c_str());
+    fprintf(stderr, "failed to open db, err: %s\n", s.ToString().c_str());
+    assert(false);
+  }
+  rocksdb::WriteOptions write_opts;
+  std::string key = "ttl_1";
+  std::string put_val = "write with ttl";
+  s = db->Put(write_opts, key, put_val);
+  if (!s.ok()) {
+    fprintf(stderr, "failed to write data into db, err: %s\n", s.ToString().c_str());
     assert(false);
   }
 
-  rocksdb::WriteOptions write_opt;
-  std::string key = "test_merge";
-  std::string val = "1";
-  s = db->Merge(write_opt, key, val);
-  assert(s.ok());
+  printf("start to sleep\n");
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+  printf("end to sleep\n");
 
-  s = db->Merge(write_opt, key, val);
-  assert(s.ok());
+  std::string val{};
+  rocksdb::ReadOptions read_opt;
+  s = db->Get(read_opt, key, &val);
+  if (!s.ok()) {
+    fprintf(stderr, "failed to get data into db, err: %s\n", s.ToString().c_str());
+    assert(false);
+  }
 
- std::string get_val = "";
-  s = db->Get(rocksdb::ReadOptions(), key, &get_val);
-  assert(s.ok());
-  fprintf(stdout, "val: %s\n", get_val.c_str());
+  printf("key: %s, val: %s\n", key.c_str(), val.c_str());
+
+  std::this_thread::sleep_for(std::chrono::seconds(4));
+  // do compaction, because expired entries maybe return before compaction,
+  // so wo do compaction manually and the expired entries could been removed.
+  std::string cp_key{};
+  key.copy(cp_key.data(), key.length());
+  printf("cp_key: %s\n", cp_key.c_str());
+
+  Slice begin(cp_key);
+  Slice end = key + "123";
+  s = db->CompactRange(rocksdb::CompactRangeOptions{}, &begin, &end);
+
+  val.clear();
+  s = db->Get(read_opt, key, &val);
+  if (!s.ok()) {
+    fprintf(stderr, "failed to get data into db, err: %s\n", s.ToString().c_str());
+    assert(false);
+  } else if (s.IsNotFound()) {
+    printf("key is not found due to expiration\n");
+  } else {
+    printf("key: %s, val: %s\n", key.c_str(), val.c_str());
+  }
   delete db;
 }
+
+// ttl db
+int main() {
+  playTtlDB();
+}
+
+// increase test case
+//int main() {
+//
+//  // open db and merge
+//  rocksdb::DB* db;
+//  rocksdb::Options opt;
+//  opt.create_if_missing = true;
+//  opt.merge_operator.reset(new Uint64AddOperator);
+//  rocksdb::Status s = rocksdb::DB::Open(opt, "storage/merge_example", &db);
+//  if (!s.ok()) {
+//    fprintf(stderr, "failed to open db, status: %s\n", s.ToString().c_str());
+//    assert(false);
+//  }
+//
+//  rocksdb::WriteOptions write_opt;
+//  std::string key = "test_merge";
+//  std::string val = "1";
+//  s = db->Merge(write_opt, key, val);
+//  assert(s.ok());
+//
+// std::string get_val = "";
+//  s = db->Get(rocksdb::ReadOptions(), key, &get_val);
+//  assert(s.ok());
+//  fprintf(stdout, "val: %s\n", get_val.c_str());
+//  delete db;
+//}
 
---
+++ examples/play_transaction_example.cc
@@ -0,0 +1,104 @@
+//
+// Created by 赵友清 on 2022/9/12.
+//
+
+#include "rocksdb/db.h"
+#include "rocksdb/options.h"
+#include "rocksdb/utilities/transaction_db.h"
+
+const std::string kDBPath = "storage/transaction";
+
+inline bool init_db(std::unique_ptr<rocksdb::TransactionDB>* db_prt) {
+  rocksdb::TransactionDB* db = nullptr;
+  rocksdb::Options opts{};
+  rocksdb::TransactionDBOptions transaction_opt{};
+  opts.create_if_missing = true;
+  rocksdb::Status s = rocksdb::TransactionDB::Open(
+      opts, transaction_opt, kDBPath, &db);
+  if (!s.ok()) {
+    fprintf(stderr, "failed to open db, err: %s\n", s.ToString().c_str());
+    return false;
+  } else {
+    db_prt->reset(db);
+    return true;
+  }
+}
+
+void play_transaction() {
+  std::unique_ptr<rocksdb::TransactionDB> db_ptr{};
+  bool ok = init_db(&db_ptr);
+  assert(ok);
+
+  ////////////////////////////////////////////////////////
+  //
+  // Simple Transaction Example ("Read Committed")
+  //
+  ////////////////////////////////////////////////////////
+  rocksdb::WriteOptions w_opts{};
+
+  // write data;
+  rocksdb::WriteBatch write_batch{};
+  std::vector<std::string> keys{};
+  keys.reserve(10);
+  for (int i = 0; i < 10; ++i) {
+    std::string key = std::to_string(i);
+    keys.push_back(key);
+    std::string val = "value_" + key;
+    printf("key: %s, val: %s, vector: %s\n", key.c_str(), val.c_str(), keys[i].c_str());
+    write_batch.Put(key, val);
+  }
+  rocksdb::Status s = db_ptr->Write(w_opts, &write_batch);
+  assert(s.ok());
+
+  rocksdb::ReadOptions r_opts{};
+  for (int i = 0; i < 10; ++i) {
+    std::string val{};
+    s = db_ptr->Get(r_opts, keys[i], &val);
+    printf("get from db, key: %s, val: %s\n", keys[i].c_str(), val.c_str());
+  }
+  printf("####### start transaction ############\n");
+
+  // Start a transaction
+  rocksdb::Transaction* txn = db_ptr->BeginTransaction(rocksdb::WriteOptions{});
+  assert(txn);
+  txn->Put(keys[0], keys[0] + "_txn");
+
+  std::string val_0{};
+  s = db_ptr->Get(r_opts, keys[0], &val_0);
+  assert(s.ok());
+  printf("get data outside the transaction, key: %s, val: %s\n", keys[0].c_str(), val_0.c_str());
+
+  // update key[1] outside this transaction
+  s = db_ptr->Put(w_opts, keys[1], keys[1] + "_txn");
+
+  // get key[1] lastest value due to the read commit level
+  std::string val_1{};
+  s = txn->Get(r_opts, keys[1], &val_1);
+  assert(s.ok());
+  printf("get data in this transaction after outside update, key: %s, val: %s\n", keys[1].c_str(), val_1.c_str());
+
+  // commit transaction
+  s = txn->Commit();
+  assert(s.ok());
+
+  printf("####### transaction end ############\n");
+  for (int i = 0; i < 10; ++i) {
+    std::string val{};
+    s = db_ptr->Get(r_opts, keys[i], &val);
+    printf("get from db after transaction, key: %s, val: %s\n", keys[i].c_str(), val.c_str());
+  }
+  delete txn;
+  keys.clear();
+}
+
+
+// build cmd
+// linux
+// g++-7 merge_example.cc ../librocksdb.a -o merge_example.out -I ../include/ -I ../ -std=c++17 -lpthread -lz  -lbz2 -lzstd -ldl -lsnappy -llz4  -DROCKSDB_PLATFORM_POSIX -fno-rtti
+//
+// macos
+// clang++ merge_example.cc ../librocksdb.a -o merge_example.out -I ../include/ -I ../ -std=c++17 -lpthread -lz  -lbz2 -lzstd -ldl   -DROCKSDB_PLATFORM_POSIX -fno-rtti -DOS_MACOSX
+
+int main() {
+  play_transaction();
+}
